9c8db7d6473656aef20dd6cde17d3db72004bec8
Distaid <88453698+Distaid@users.noreply.github.com>
2021-11-15 21:43:46 +0300

    Ковертер из объекта в строку

---
+++ AIDMusicApp/Converters/ObjectToStringConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace AIDMusicApp.Converters
+{
+    class ObjectToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return null;
+
+            return value.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
