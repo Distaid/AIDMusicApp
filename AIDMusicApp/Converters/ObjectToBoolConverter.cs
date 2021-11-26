0021cbb4fbf0b85bb5d54c382d1af4d1c4013b78
Distaid <88453698+Distaid@users.noreply.github.com>
2021-11-26 12:34:17 +0300

    Ковертеры

---
+++ AIDMusicApp/Converters/ObjectToBoolConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace AIDMusicApp.Converters
+{
+    public class ObjectToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+                return true;
+
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
---
+++ AIDMusicApp/Converters/SongToStringConverter.cs
@@ -0,0 +1,38 @@
+﻿using AIDMusicApp.Models;
+using System;
+using System.Linq;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Collections.ObjectModel;
+
+namespace AIDMusicApp.Converters
+{
+    public class SongToStringConverter : IMultiValueConverter
+    {
+
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values == null)
+                return null;
+
+            var name = (string)values[0];
+            var guests = values[1] as ObservableCollection<Musician>;
+
+            if (guests != null)
+                if (guests.Count != 0)
+                {
+                    name += " (feat ";
+                    name += string.Join(", ", guests.Select((m) => m.Name));
+                    name += ")";
+                }
+
+            return name;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
